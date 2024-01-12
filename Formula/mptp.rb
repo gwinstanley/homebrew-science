2b40dda1d7e36abf64f3657eb88d90b970f44c79
Giles Winstanley <g.winstanley@snaq.net>
2024-01-12 15:23:07 +0000

    mPTP 0.2.5 (updated formula).

--- Formula/mptp.rb
+++ Formula/mptp.rb
@@ -2,9 +2,12 @@ class Mptp < Formula
   # cite Kapli_2017: "https://doi.org/10.1093/bioinformatics/btx025"
   desc "mPTP - a tool for single-locus species delimitation"
   homepage "https://github.com/Pas-Kapli/mptp"
-  url "https://github.com/Pas-Kapli/mptp/archive/v0.2.4.tar.gz"
-  sha256 "31467d9a98356679f5ee0f417fc8b6b76a0cd3743bd57a29ebff8b43c201abd6"
+  url "https://github.com/Pas-Kapli/mptp/archive/v0.2.5.tar.gz"
+  sha256 "403cda3243ff45939ca9c5099e7d71f917f648a2cb49dfcc80843c0c37db9add"
   head "https://github.com/Pas-Kapli/mptp.git"
+
+  # Fix reporting of macOS aarch64
+  patch :DATA
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -13,16 +16,34 @@ class Mptp < Formula
   depends_on "gsl" => :recommended
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make"
-    ENV.deparallelize
-    system "make", "install"
+    ENV.O3
+    ENV.deparallelize do
+      system "./autogen.sh"
+      system "./configure", "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}"
+      system "make"
+      system "make", "install"
+    end
   end
 
   test do
     assert_match /#{version}/, shell_output("#{bin}/mptp --version")
   end
 end
+
+__END__
+diff --git a/src/mptp.h b/src/mptp.h
+index 77629d9..0480b08 100644
+--- a/src/mptp.h
++++ b/src/mptp.h
+@@ -65,6 +65,9 @@
+
+ #ifdef __APPLE__
+ #define PROG_OS "osx"
++#ifdef __aarch64__
++#define PROG_CPU "aarch64"
++#endif
+ #include <sys/resource.h>
+ #include <sys/sysctl.h>
+ #endif
